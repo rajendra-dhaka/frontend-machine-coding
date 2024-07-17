b5e9ff043dcd9c7355d75f343aa624e427597d6f
rajendra-dhaka <rajendra@bluepi.in>
2024-07-17 22:41:17 +0530

    react toast initiated

--- src/app/react-toast/page.tsx
+++ src/app/react-toast/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+const colorClasses = {
+  red: "bg-red-600",
+  blue: "bg-blue-600",
+  green: "bg-green-600",
+  // Add other colors as needed
+};
+
+const Button = ({ text, color }) => {
+  return <button className={`py-1 px-4 ${colorClasses.red}`}>{text}</button>;
+};
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col gap-2 items-center">
+        <Button text="Raja" color={"red"} />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
